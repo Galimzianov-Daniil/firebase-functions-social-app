0bbeae9e14694ae56d8bc669a0d86019fcf4224c
Galimzianov Daniil <daniilgalimzianov4dev@gmail.com>
2020-08-19 22:56:32 +0500

    Removed config file

--- utils/config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    apiKey: "AIzaSyAi9Y-k7FKdHL-d3TwN0e5pFPZRIkwUEW8",
-    authDomain: "twitter-clone-bf6fc.firebaseapp.com",
-    databaseURL: "https://twitter-clone-bf6fc.firebaseio.com",
-    projectId: "twitter-clone-bf6fc",
-    storageBucket: "twitter-clone-bf6fc.appspot.com",
-    messagingSenderId: "414576358741",
-    appId: "1:414576358741:web:17cc3da3e9545babbe6bdc"
-};
